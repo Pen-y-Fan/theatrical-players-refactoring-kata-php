ae8288d2818d695847f02f27d0bb6940b243d37a
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2020-05-17 19:15:02 +0100

    Added getAmount to PerformanceCalculator class

--- php/src/PerformanceCalculator.php
+++ php/src/PerformanceCalculator.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Theatrical;
 
+use Error;
 
 class PerformanceCalculator
 {
@@ -26,5 +27,27 @@ class PerformanceCalculator
     {
         $this->performance = $performance;
         $this->play = $play;
+    }
+
+    public function getAmount()
+    {
+        switch ($this->play->type) {
+            case "tragedy":
+                $result = 40000;
+                if ($this->performance->audience > 30) {
+                    $result += 1000 * ($this->performance->audience - 30);
+                }
+                break;
+            case "comedy":
+                $result = 30000;
+                if ($this->performance->audience > 20) {
+                    $result += 10000 + 500 * ($this->performance->audience - 20);
+                }
+                $result += 300 * $this->performance->audience;
+                break;
+            default:
+                throw new Error("unknown type: {$this->play->type}");
+        }
+        return $result;
     }
 }
