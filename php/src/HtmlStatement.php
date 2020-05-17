1ca6d1ce716a9e9450b4c1cc9ec831a181bf9c7e
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2020-05-17 15:47:29 +0100

    Move render usd method to HtmlStatement

--- php/src/HtmlStatement.php
+++ php/src/HtmlStatement.php
@@ -23,7 +23,7 @@ class HtmlStatement
                 $result .= "<td>{$this->usd($performance->amount)}</td></tr>\n";
             }
         $result .= "</table>\n";
-        $result .= "<p>Amount owed is <em>{$data->totalAmount}</em></p>\n";
+        $result .= "<p>Amount owed is <em>{$this->usd($data->totalAmount)}</em></p>\n";
         $result .= "<p>You earned <em>{$data->totalVolumeCredits}</em> credits</p>\n";
         return $result;
     }
