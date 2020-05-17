c5d2126da8222b498a269370fc0280106e777cc5
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2020-05-17 19:58:57 +0100

    getAmount Polymorphism refactor

--- php/src/PerformanceCalculator.php
+++ php/src/PerformanceCalculator.php
@@ -31,24 +31,7 @@ class PerformanceCalculator
 
     public function getAmount(): int
     {
-        switch ($this->play->type) {
-            case "tragedy":
-                $result = 40000;
-                if ($this->performance->audience > 30) {
-                    $result += 1000 * ($this->performance->audience - 30);
-                }
-                break;
-            case "comedy":
-                $result = 30000;
-                if ($this->performance->audience > 20) {
-                    $result += 10000 + 500 * ($this->performance->audience - 20);
-                }
-                $result += 300 * $this->performance->audience;
-                break;
-            default:
-                throw new Error("unknown type: {$this->play->type}");
-        }
-        return $result;
+        Throw new Error('subclass responsibility');
     }
 
     public function volumeCredit(): int
--- php/src/TragedyCalculator.php
+++ php/src/TragedyCalculator.php
@@ -4,24 +4,14 @@ declare(strict_types=1);
 
 namespace Theatrical;
 
-use Error;
-
 class TragedyCalculator extends PerformanceCalculator
 {
     public function getAmount(): int
     {
-        switch ($this->play->type) {
-            case "tragedy":
-                $result = 40000;
-                if ($this->performance->audience > 30) {
-                    $result += 1000 * ($this->performance->audience - 30);
-                }
-                return $result;
-            case "comedy":
-                Throw new Error("Bad Thing");
-                break;
-            default:
-                Throw new Error("unknown type: {$this->play->type}");
+        $result = 40000;
+        if ($this->performance->audience > 30) {
+            $result += 1000 * ($this->performance->audience - 30);
         }
+        return $result;
     }
 }
